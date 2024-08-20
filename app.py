fb87b47fe09cced5be6d1a04e5831dec0babfbdf
Juan Cappi <jmcappi@us.ibm.com>
2024-08-20 15:35:02 -0300

    first version of demo

---
+++ app.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import joblib
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+
+# Load the trained logistic regression model
+log_reg_model = joblib.load('model/logistic_regression_model_8.pkl')
+# Load the scalers and encoders
+scaler = joblib.load('model/scaler.pkl')
+label_encoders = joblib.load('model/label_encoders.pkl')
+
+# Streamlit form
+st.title("Prueba de concepto")
+st.header("Modelo predictivo")
+st.write("Probabilidad de que al menos 8 folículos lleguen a Metafase II")
+
+with st.form("input_form"):
+    age = st.number_input("Edad paciente", min_value=15, max_value=70, value=30)
+    NAF = st.number_input("Cantidad de folículos aspirados", min_value=0, max_value=1000, value=0)
+    IMC = st.number_input("Indice de masa corporal", min_value=0.0, value=0.0)
+    CFA = st.number_input("Conteo folicular antral", min_value=0.0, value=0.0)
+    HAM = st.number_input("HAM", min_value=0.0, value=0.0)
+    
+    prev_child = st.checkbox("Tuvo hijos anteriormente?")
+    smokes = st.selectbox("Fuma?", ["si", "no", "ex fumadora"])
+
+    submit_button = st.form_submit_button(label="Predecir")
+
+if submit_button:
+    # Prepare the input data
+    input_data = pd.DataFrame({
+        '9 Edad paciente': [age],
+        '38 Cantidad de folículos aspirados': [NAF],
+        'IMC': [IMC],
+        '25 CFA': [CFA],
+        '26 HAM': [HAM],
+        'Hijos Previos': ['si' if prev_child else 'no'],
+        'Fuma': [smokes]
+    })
+    
+    # Scale numeric columns
+    numeric_cols = ['9 Edad paciente', 'IMC', '25 CFA', '26 HAM', '38 Cantidad de folículos aspirados']
+    input_data[numeric_cols] = scaler.transform(input_data[numeric_cols])
+
+    # Encode non-numeric columns
+    for col in ['Hijos Previos', 'Fuma']:
+        input_data[col] = label_encoders[col].transform(input_data[col])
+
+    input_data = input_data[['9 Edad paciente', 'IMC', '25 CFA', '26 HAM', '38 Cantidad de folículos aspirados','Hijos Previos', 'Fuma']]    
+    # Predict probabilities
+    prediction = log_reg_model.predict(input_data)
+    probability = log_reg_model.predict_proba(input_data)
+
+    # Display results
+    st.write(f"Predicción: {'Positiva' if prediction[0] else 'Negativa'}")
+    st.write(probability)
+    st.write(f"Hay un {probability[0][1] * 100:.2f}% de probabilidad de que 8 o más folículos lleguen a Metafase II.")
+
